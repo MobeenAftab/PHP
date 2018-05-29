272a8c4046a3a5134ea38f566375e3a083baee52
Mobeen Aftab <mobeenaftab1@gmail.com>
2018-05-29 21:12:56 +0100

    php REST API
    Create category method

---
+++ php_REST_myblog/api/categories/create.php
@@ -0,0 +1,37 @@
+<?php
+    // REST API access through HTTP.
+
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-with');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Categories.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instanstiate blog post object.
+    $post = new Categories($db);
+
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    // $post->id = $data->id;
+    $post->name = $data->name;
+    // $post->created_at = $data->created_at;
+
+    // Create post
+    if ($post->create()) {
+        echo json_encode(
+            array('message' => 'Post Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Post Not Created')
+        );
+    }
+
--- php_REST_myblog/models/Categories.php
+++ php_REST_myblog/models/Categories.php
@@ -27,6 +27,7 @@
             return $stmt;
         }
 
+        // Get single category
         public function read_single() {
             // Create query
             $query = 'SELECT * FROM ' . $this->table . ' WHERE id = ? LIMIT 0,1';
@@ -45,5 +46,32 @@
             $this->created_at = $row['created_at'];
 
             return $stmt;
+        }
+
+        public function create() {
+            // Create query
+            $query = 'INSERT INTO ' . $this->table . '
+                SET
+                    name = :name';
+
+            $stmt = $this->conn->prepare($query);
+
+            // Clean data.
+            // $this->id = htmlspecialchars(strip_tags($this->id));
+            $this->name = htmlspecialchars(strip_tags($this->name));
+            // $this->created_at = htmlspecialchars(strip_tags($this->created_at));
+
+            // Bind data
+            // $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':name', $this->name);
+            // $stmt->bindParam(':created_at', $this->created_at);
+
+            // Execute query
+            if ($stmt->execute()) {
+                return true;
+            }
+            // print error if error
+            printf("Error: %s.\n", $stmt->error);
+            return false;
         }
     }
